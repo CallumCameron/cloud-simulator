219ca883ad7d7812ec121cd1cc7b49d1e087aec5
Callum Cameron <cjcameron7@gmail.com>
2015-02-25 12:56:18 +0000

    Refactoring.

--- cloud.js
+++ cloud.js
@@ -1,26 +1,26 @@
-var resources = [
-    {
-        name: "compute",
-        costMoney: 1,
-        costPower: 3,
-        image: "processor.png",
-        tooltip: "Computation: processors to run the code"
-    },
-    {
-        name: "network",
-        costMoney: 3,
-        costPower: 1,
-        image: "network.png",
-        tooltip: "Connectivity: network infrastructure in the data centre"
-    },
-    {
-        name: "storage",
-        costMoney: 2,
-        costPower: 2,
-        image: "harddrive.png",
-        tooltip: "Storage: hard drives and SSDs"
-    }
-];
+// var resources = [
+//     {
+//         name: "compute",
+//         costMoney: 1,
+//         costPower: 3,
+//         image: "processor.png",
+//         tooltip: "Computation: processors to run the code"
+//     },
+//     {
+//         name: "network",
+//         costMoney: 3,
+//         costPower: 1,
+//         image: "network.png",
+//         tooltip: "Connectivity: network infrastructure in the data centre"
+//     },
+//     {
+//         name: "storage",
+//         costMoney: 2,
+//         costPower: 2,
+//         image: "harddrive.png",
+//         tooltip: "Storage: hard drives and SSDs"
+//     }
+// ];
 
 var initResourceCounter = function() {
     var counter = {};
@@ -127,43 +127,43 @@ var updatePacketSpawners = function() {
 };
 
 
-var createSlider = function(name, min, max, step, image, tooltip, widthPercent) {
-    var column = $("<div>").addClass("panel-column").attr("style", "width: " + widthPercent + "%");
-    var slider = $("<div>").addClass("slider-slider").attr("id", name + "-slider");
-    var text = $("<p>").addClass("slider-text").attr("id", name + "-text").text("0");
-    var img = $("<img>").addClass("slider-img").attr("src", image).attr("title", tooltip);
+// var createSlider = function(name, min, max, step, image, tooltip, widthPercent) {
+//     var column = $("<div>").addClass("panel-column").attr("style", "width: " + widthPercent + "%");
+//     var slider = $("<div>").addClass("slider-slider").attr("id", name + "-slider");
+//     var text = $("<p>").addClass("slider-text").attr("id", name + "-text").text("0");
+//     var img = $("<img>").addClass("slider-img").attr("src", image).attr("title", tooltip);
 
-    column.append(slider, text, img);
+//     column.append(slider, text, img);
 
-    slider.slider({
-        orientation: "vertical",
-        range: "min",
-        min: min,
-        max: max,
-        value: min,
-        step: step,
-        slide: function(event, ui) {
-            text.text(slider.slider("value"));
-        },
-        change: function(event, ui) {
-            text.text(slider.slider("value"));
-        }
-    });
+//     slider.slider({
+//         orientation: "vertical",
+//         range: "min",
+//         min: min,
+//         max: max,
+//         value: min,
+//         step: step,
+//         slide: function(event, ui) {
+//             text.text(slider.slider("value"));
+//         },
+//         change: function(event, ui) {
+//             text.text(slider.slider("value"));
+//         }
+//     });
 
-    text.text(slider.slider("value"));
+//     text.text(slider.slider("value"));
 
-    return column;
-};
+//     return column;
+// };
 
-var createLoadBox = function(name, unit, widthPercent) {
-    return $("<div>").addClass("panel-column").attr("style", "width: " + widthPercent + "%").append(
-        $("<div>").addClass("load-box-colour").attr("id", name + "-load-box"),
-        $("<p>").addClass("load-box-text").append(
-            $("<span>").attr("id", name + "-load-text").text("0"),
-            unit
-        )
-    );
-};
+// var createLoadBox = function(name, unit, widthPercent) {
+//     return $("<div>").addClass("panel-column").attr("style", "width: " + widthPercent + "%").append(
+//         $("<div>").addClass("load-box-colour").attr("id", name + "-load-box"),
+//         $("<p>").addClass("load-box-text").append(
+//             $("<span>").attr("id", name + "-load-text").text("0"),
+//             unit
+//         )
+//     );
+// };
 
 var frameNum = 0;
 
@@ -210,35 +210,35 @@ var updateCloud = function() {
     $("#total-cost-energy").text(totalEnergyUsed);
 };
 
-var redAmberGreen = function(percent) {
-    var h = 0;
+// var redAmberGreen = function(percent) {
+//     var h = 0;
 
-    if (percent < 50) {
-        h = 120;
-    }
-    else if (percent > 100) {
-        h = 0;
-    }
-    else {
-        h = Math.round((1 - ((percent - 50) / 50)) * 120);
-    }
+//     if (percent < 50) {
+//         h = 120;
+//     }
+//     else if (percent > 100) {
+//         h = 0;
+//     }
+//     else {
+//         h = Math.round((1 - ((percent - 50) / 50)) * 120);
+//     }
 
-    return h;
-}
+//     return h;
+// };
 
-var updateLoad = function() {
-    var updateIndividualLoad = function(name) {
-        var load = Math.round(demand[name] / provision[name] * 100);
-        $("#" + name + "-load-text").text(Math.min(load, 100));
+// var updateLoad = function() {
+//     var updateIndividualLoad = function(name) {
+//         var load = Math.round(demand[name] / provision[name] * 100);
+//         $("#" + name + "-load-text").text(Math.min(load, 100));
 
-        var h = redAmberGreen(load);
-        $("#" + name + "-load-box").attr("style", "background-color: hsl(" + h + ", 80%, 50%);");
-    };
+//         var h = redAmberGreen(load);
+//         $("#" + name + "-load-box").attr("style", "background-color: hsl(" + h + ", 80%, 50%);");
+//     };
 
-    for (var i = 0; i < resources.length; i++) {
-        updateIndividualLoad(resources[i].name);
-    }
-};
+//     for (var i = 0; i < resources.length; i++) {
+//         updateIndividualLoad(resources[i].name);
+//     }
+// };
 
 var updateResponseTime = function() {
     // With N resources, N*100% is full load
@@ -303,26 +303,305 @@ var setupCloudColumn = function(resource) {
         resource.tooltip + ". One unit costs: £" + resource.costMoney + "/s, " + resource.costPower + " W.",
         width
     ));
-}
+};
 
-var main = function() {
-    $("#response-section").append(createLoadBox("response", " ms", 100));
 
-    for (var i = 0; i < clientTypes.length; i++) {
-        setupClientColumn(clientTypes[i]);
+$(document).ready(function() {
+    // UI
+
+    function Slider(min, max, step, image, tooltip, widthPercent) {
+        var column = $("<div>").addClass("panel-column").attr("style", "width: " + widthPercent + "%");
+        var slider = $("<div>").addClass("slider-slider");
+        var text = $("<p>").addClass("slider-text").text("0");
+        var img = $("<img>").addClass("slider-img").attr("src", image).attr("title", tooltip);
+
+        column.append(slider, text, img);
+
+        slider.slider({
+            orientation: "vertical",
+            range: "min",
+            min: min,
+            max: max,
+            value: min,
+            step: step,
+            slide: function(event, ui) {
+                text.text(slider.slider("value"));
+            },
+            change: function(event, ui) {
+                text.text(slider.slider("value"));
+            }
+        });
+
+        text.text(slider.slider("value"));
+
+        return {
+            addTo: function(parent) {
+                parent.append(column);
+                return this;
+            },
+            getValue: function() {
+                return slider.slider("value");
+            },
+            setValue: function(val) {
+                slider.slider("value", val);
+                return this;
+            },
+            reset: function() {
+                this.setValue(min);
+                return this;
+            }
+        };
     }
 
-    for (var i = 0; i < resources.length; i++) {
-        setupCloudColumn(resources[i]);
+    function LoadBox(unit, widthPercent) {
+        var value = 0;
+        var hundredPercentValue = 100;
+        var text = $("<span>").text(value);
+        var colourBox = $("<div>").addClass("load-box-colour");
+        var column = $("<div>").addClass("panel-column").attr("style", "width: " + widthPercent + "%").append(
+            colourBox,
+            $("<p>").addClass("load-box-text").append(
+                text,
+                unit
+            )
+        );
+
+        var loadBox = {
+            addTo: function(parent) {
+                parent.append(column);
+                return this;
+            },
+            getValue: function() {
+                return value;
+            },
+            setValue: function(val, hundredPercent) {
+                value = val;
+                if (hundredPercent) {
+                    hundredPercentValue = hundredPercent;
+                }
+
+                text.text(value);
+
+                // Update the colour of the box
+                var percent = (value / hundredPercentValue) * 100.0;
+                var hue = 0;
+
+                if (percent < 50) {
+                    hue = 120;
+                }
+                else if (percent > 100) {
+                    hue = 0;
+                }
+                else {
+                    hue = Math.round((1 - ((percent - 50) / 50)) * 120);
+                }
+
+                colourBox.attr("style", "background-color: hsl(" + hue + ", 100%, 50%);");
+                return this;
+            },
+            reset: function() {
+                this.setValue(0);
+                return this;
+            }
+        };
+
+        loadBox.setValue(value, hundredPercentValue);
+        return loadBox;
     }
 
-    $("button").button();
-    $(document).tooltip();
+    function Cost(money, power) {
+        return {
+            getMoney: function() {
+                return money;
+            },
+            getPower: function() {
+                return power;
+            },
+            increaseBy: function(other) {
+                money += other.getMoney();
+                power += other.getPower();
+                return this;
+            },
+            decreaseBy: function(other) {
+                money -= other.getMoney();
+                power -= other.getPower();
+                return this;
+            },
+            times: function(factor) {
+                return Cost(money * factor, power * factor);
+            },
+            reset: function() {
+                money = 0;
+                power = 0;
+                return this;
+            }
+        };
+    }
 
-    $(".failure-button").click(equipmentFailure);
+    function Resource(name, unitCost, image, tooltip) {
+        var width = 33;
+        var loadBox = LoadBox("%", width).addTo($("#load-section"));
+        var slider = Slider(
+            10,
+            100,
+            10,
+            image,
+            tooltip + ". One unit costs: £" + unitCost.getMoney() + "/s, " + unitCost.getPower() + " W.",
+            width
+        ).addTo($("#provision-section"));
 
-    update();
-    setInterval(update, 100);
-}
+        var demandSources = [];
+        var loadPercent = 0;
 
-$(document).ready(main);
+        return {
+            getName: function() {
+                return name;
+            },
+            addDemandSource: function(source) {
+                demandSources.push(source);
+                return this;
+            },
+            tick: function() {
+                var demand = 0;
+                for (var i = 0; i < demandSources.length; i++) {
+                    demand += demandSources[i]();
+                }
+
+                loadPercent = Math.round(demand / slider.getValue() * 100);
+                colourBox.setValue(Math.min(loadPercent, 100));
+                return this;
+            },
+            getLoadPercent: function() {
+                return loadPercent;
+            },
+            getTickCost: function() {
+                return unitCost.times(slider.getValue());
+            },
+            reset: function() {
+                loadBox.reset();
+                slider.reset();
+                return this;
+            }
+        };
+    }
+
+    // var currentMode = null;
+
+    // var mainTimer = {
+    //     frameNum: 0,
+    //     intervalID: null,
+    //     start: function() {
+    //         if (this.intervalID === null) {
+    //             var that = this;
+    //             var tick = function() {
+    //                 currentMode.tick(that.frameNum);
+    //                 that.frameNum++;
+    //             };
+    //             tick();
+    //             this.intervalID = setInterval(tick, 100);
+    //         }
+    //     },
+    //     pause: function() {
+    //         if (this.intervalID !== null) {
+    //             clearInterval(this.intervalID);
+    //             this.intervalID = null;
+    //         }
+    //     },
+    //     reset: function() {
+    //         this.pause();
+    //         this.frameNum = 0;
+    //     }
+    // };
+
+
+    // function Mode(button, enter, firstEnter, exit, tick) {
+    //     var mode = {
+    //         firstTime: true,
+    //         enter: function() {
+    //             enter();
+    //             if (this.firstTime) {
+    //                 firstEnter();
+    //                 this.firstTime = false;
+    //             }
+    //         },
+    //         exit: exit,
+    //         tick: tick
+    //     };
+    //     button.click(function() { changeMode(mode); });
+    //     return mode;
+    // }
+
+    // function changeMode(newMode) {
+    //     if (newMode !== currentMode) {
+    //         mainTimer.reset();
+    //         if (currentMode !== null) {
+    //             currentMode.exit();
+    //         }
+    //         currentMode = newMode;
+    //         currentMode.enter();
+    //         mainTimer.start();
+    //     }
+    // }
+
+
+    // var manualMode = Mode(
+    //     "#btn-mode-manual",
+    //     function() {},
+    //     function() {},
+    //     function() {},
+    //     function(frameNum) {}
+    // );
+
+    // var responseMode = Mode(
+    //     "#btn-mode-response",
+    //     function() {},
+    //     function() {},
+    //     function() {},
+    //     function(frameNum) {}
+    // );
+
+    // var gameMode = Mode(
+    //     "#btn-mode-game",
+    //     function() {},
+    //     function() {},
+    //     function() {},
+    //     function(frameNum) {}
+    // );
+
+
+
+
+
+
+
+
+
+
+    // changeMode(manualMode);
+
+
+
+    var resources = [
+        Resource("compute", Cost(1, 3), "processor.png", "Computation: processors to run the code"),
+        Resource("network", Cost(3, 1), "network.png", "Connectivity: network infrastructure in the data centre"),
+        Resource("storage", Cost(2, 2), "harddrive.png", "Storage: hard drives and SSDs")
+    ];
+
+    // $("#response-section").append(createLoadBox("response", " ms", 100));
+
+    // for (var i = 0; i < clientTypes.length; i++) {
+    //     setupClientColumn(clientTypes[i]);
+    // }
+
+    // for (var i = 0; i < resources.length; i++) {
+    //     setupCloudColumn(resources[i]);
+    // }
+
+    // $("button").button();
+    // $(document).tooltip();
+
+    // $(".failure-button").click(equipmentFailure);
+
+    // update();
+    // setInterval(update, 100);
+});
